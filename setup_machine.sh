65828c497b91c3fe05a678a06492459af4c45960
Ted Steen <ted.steen@gmail.com>
2025-05-06 15:23:11 +0100

    keep it simple

--- setup_machine.sh
+++ setup_machine.sh
@@ -2,4 +2,4 @@
 export TARGET_CONFIG=${1:-"test"}
 export TARGET_HOST=${2:-"nixos"}@${3:-"<missing-ip>"}
 
-nix run github:nix-community/nixos-anywhere/1.9.0 -- --generate-hardware-config nixos-generate-config ./$TARGET_CONFIG/hardware-configuration.nix --flake .#$TARGET_CONFIG --build-on remote $TARGET_HOST --target-host $TARGET_HOST
+nix run github:nix-community/nixos-anywhere/1.9.0 -- --generate-hardware-config nixos-generate-config ./$TARGET_CONFIG/hardware-configuration.nix --flake ./$TARGET_CONFIG#$TARGET_CONFIG --build-on remote $TARGET_HOST --target-host $TARGET_HOST
--- update_machine.sh
+++ update_machine.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 export TARGET_CONFIG=${1:-"test"}
 export TARGET_HOST=${2:-"nixos"}@${3:-"<missing-ip>"}
-nixos-rebuild --fast --build-host $TARGET_HOST --use-remote-sudo --flake .#$TARGET_CONFIG --target-host $TARGET_HOST switch
+nix-shell -p '(nixos{}).nixos-rebuild' git --run "nixos-rebuild --fast --build-host $TARGET_HOST --use-remote-sudo --flake ./$TARGET_CONFIG#$TARGET_CONFIG --target-host $TARGET_HOST switch"
+
