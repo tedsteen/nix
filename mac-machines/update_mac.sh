209747322926b2c8927602738d5609e3b441a325
Ted Steen <ted.steen@gmail.com>
2025-05-18 15:53:53 +0100

    make it possible to setup different macs

--- mac-machines/update_mac.sh
+++ mac-machines/update_mac.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+MAC_MACHINE=${1:-"teds-mbp"}
 export NIX_CONFIG="extra-experimental-features = nix-command flakes"
-nix run github:lnl7/nix-darwin -- switch --flake ./#"teds-mbp"
+nix run github:lnl7/nix-darwin -- switch --flake ./#$MAC_MACHINE
