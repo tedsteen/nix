0712aa5d09da323710bfe6dc0a62882e572a5c1c
Ted Steen <ted.steen@gmail.com>
2025-06-13 10:09:43 +0100

    default to the mac you're running from when doing ./update_mac.sh

--- mac-machines/update_mac.sh
+++ mac-machines/update_mac.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-MAC_MACHINE=${1:-"teds-mbp"}
+MAC_MACHINE=${1:-$(hostname)}
 export NIX_CONFIG="extra-experimental-features = nix-command flakes"
 nix run github:lnl7/nix-darwin -- switch --flake ./#$MAC_MACHINE
