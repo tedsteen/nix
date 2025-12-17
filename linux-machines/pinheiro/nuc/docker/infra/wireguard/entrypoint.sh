02cc2486151aea50640d7eaba2603414bd571a72
Ted Steen <ted.steen@gmail.com>
2025-12-17 17:31:11 +0000

    fix wireguard

--- linux-machines/pinheiro/nuc/docker/infra/wireguard/entrypoint.sh
+++ linux-machines/pinheiro/nuc/docker/infra/wireguard/entrypoint.sh
@@ -1,28 +1,18 @@
 #!/bin/sh
+set -euo pipefail
+
 WAN_IF=$(ip route show default | awk '/default/ {print $5}')
 WG_IF="wg0"
+WG_CONF="/etc/wireguard/wg0.conf"
 
-if [ ! -f "/etc/wireguard/wg0.conf" ]; then
-    echo "Wireguard config not mounted to /etc/wireguard/wg0.conf" >&2
-    exit 1
-fi
-
-echo "Setting DNS server(s) to $DNS_SERVERS"
-echo $DNS_SERVERS | tr ',' '\n' | while read server; do echo "nameserver $server"; done > /etc/resolv.conf
-
-echo "Starting Wireguard $interface"
-wg-quick up $WG_IF
+echo "Starting Wireguard $WG_IF"
+wg-quick up "$WG_IF"
 
 echo 'Enabling source NAT'
 iptables --table nat --append POSTROUTING --out-interface $WG_IF -j MASQUERADE
 #iptables --append FORWARD --in-interface $WAN_IF -j ACCEPT
 iptables -A FORWARD -i "$WAN_IF" -o "$WG_IF" -j ACCEPT
 iptables -A FORWARD -i "$WG_IF" -o "$WAN_IF" -j ACCEPT
-
-if [ ! -z "$PORTFORWARD_PORT" ] && [ ! -z "$PORTFORWARD_IPADDRESS" ]; then
-    echo "Setting up destination NAT from port $PORTFORWARD_PORT to $PORTFORWARD_IPADDRESS:$PORTFORWARD_PORT"
-    iptables -A PREROUTING -t nat -i $WG_IF -p tcp --dport $PORTFORWARD_PORT -j DNAT --to $PORTFORWARD_IPADDRESS:$PORTFORWARD_PORT
-fi
 
 # Handle shutdown behavior
 finish () {
